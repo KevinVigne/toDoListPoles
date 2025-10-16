65cde54b6f1d2464698a23fb051c1c81384635d0
KevinVigne <kev.vigne@gmail.com>
2025-10-16 14:04:08 +0200

    Création de la Vue de l'ajout d'une tâche et affichage de la vue

--- src/Models/Task.php
+++ src/Models/Task.php
@@ -1,7 +1,8 @@
 <?php
-    namespace App\Models;
+namespace App\Models;
 
-class Task{
+class Task
+{
     private ?string $id;
     private ?string $title;
     private ?string $description;
@@ -9,63 +10,65 @@ class Task{
     private ?string $creation_date;
     private ?string $modification_date;
 
-    public function __construct(?string $id,?string $title,?string $description,?string $status,?string $creation_date,?string $modification_date){
+    public function __construct(?string $id, ?string $title, ?string $description, ?string $status, ?string $creation_date, ?string $modification_date)
+    {
         $this->id = $id;
         $this->title = $title;
         $this->description = $description;
-        $this->status =$status;
-        $this->creation_date = $creation_date;
-        $this->modification_date =$modification_date;
-    }
-
-    // SET 
-    public function setId(string $id):void
-    {
-        $this->id = $id;
-    }
-    public function setTitle(string $title):void
-    {
-        $this->title = $title;
-    }
-    public function setDescription(string $description):void
-    {
-        $this->description = $description;
-    }
-    public function setStatus(string $status):void
-    {
         $this->status = $status;
-    }
-    public function setCreationDate(string $creation_date):void
-    {
-        $this->create_date = $creation_date;
-    }
-    public function setModificationDate(string $modification_date):void
-    {
+        $this->creation_date = $creation_date;
         $this->modification_date = $modification_date;
     }
-    // GET
-    public function getId():?string
+
+    //les getteurs
+    public function getId(): ?string
     {
         return $this->id;
     }
-    public function getTitle():?string
+    public function getTitle(): ?string
     {
         return $this->title;
     }
-    public function getDescription():?string
+    public function getDescription(): ?string
     {
         return $this->description;
     }
-    public function getStatus():?string
+    public function getStatus(): ?string
     {
         return $this->status;
     }
-    public function getCreationDate():?string
+    public function getCreationDate(): ?string
     {
         return $this->creation_date;
     }
-    public function getModificationDate():?string
+    public function getModificationDate(): ?string
     {
         return $this->modification_date;
+    }
+
+    //les setteurs
+    public function setId(string $id)
+    {
+        $this->id = $id;
+    }
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+    public function setCreationDate(string $creation_date): void
+    {
+        $this->creation_date = $creation_date;
+    }
+    public function setModificationDate(string $modification_date): void
+    {
+        $this->modification_date = $modification_date;
     }
 }
